740b58c0f96c1baef37ca32bc8ae803d8c189f2b
rakinplaban <rakinshahriar54@gmail.com>
2024-05-22 18:24:30 +0600

    Perform unit testing
    Co-authored-by: smirk235 <170498037+smirk235@users.noreply.github.com>

---
+++ src/rod_paper_seassor/Game.java
@@ -0,0 +1,54 @@
+package rod_paper_seassor;
+import java.util.*;
+
+public class Game {
+	private int term;
+	private int bot_score = 0;
+	private int player_score = 0;
+	public Game() {
+		System.out.println("Choose any one of them\n"
+				+ "0----- Rock\n"
+				+ "1----- Paper\n"+
+				"2----- Seassor\n"
+				+"Other--- Negative marking");
+	}
+	
+	public int random() {
+		Random random_pc_input = new Random();
+		int a = random_pc_input.nextInt(3);
+		return a;
+	}
+	
+	public void play(int term) {
+		int bot_term;
+		bot_term = random();
+		if(bot_term == this.term) {
+			System.out.println();
+		}
+		if(bot_term == 0 && this.term == 1) {
+			player_score++;
+		}else if(bot_term == 0 && this.term == 2) {
+			bot_score++;
+		}else if(bot_term == 1 && this.term == 2) {
+			player_score++;
+		}else if(bot_term == 1 && this.term == 0) {
+			bot_score++;
+		}else if(bot_term == 2 && this.term == 0) {
+			player_score++;
+		}else if(bot_term == 2 && this.term == 1) {
+			bot_score++;
+		}else {
+			player_score--;
+		}
+	}
+	
+	public void winner_determiner() {
+		if(bot_score > player_score) {
+			System.out.println("Sorry, you lost the game!");
+		}else if(bot_score < player_score){
+			System.out.println("Congrats, you're the winner!");
+		}else {
+			System.out.println("Draw!");
+		}
+	}
+}
