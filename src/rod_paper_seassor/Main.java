ec6dd08663306327e840b0e26e6895dc05a25569
rakinplaban <rakinshahriar54@gmail.com>
2024-05-22 20:22:00 +0600

    Main method completed.

--- src/rod_paper_seassor/Main.java
+++ src/rod_paper_seassor/Main.java
@@ -8,11 +8,20 @@ public class Main {
 		// TODO Auto-generated method stub
 		// System.out.print("Hello World! Checking phase!");
 		try {
-			int term_selection;
+			int term_selection, i = 0;
 			System.out.println("Please enter an integer to choose number of terms");
 			Scanner input = new Scanner(System.in);
 			term_selection = input.nextInt();
 			System.out.println("You entered: "+term_selection);
+			// Creating an instance of the Game class.
+			Game new_game = new Game();
+			System.out.println("Let's start");
+			while(i<term_selection) {
+				int select = input.nextInt();
+				new_game.play(select);
+				i++;
+			}
+			new_game.winner_determiner();
 			System.exit(0);
 		}
 		catch(Exception e){
